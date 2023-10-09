be6a60e1f964f27d025d73e1f48e87fb81994673
engrabdulmalik <engrabdulmaliq@gmail.com>
2023-10-09 09:15:26 +0500

    modal

--- errormodal/src/components/UI/ErrorModal2.js
+++ errormodal/src/components/UI/ErrorModal2.js
@@ -1,7 +1,6 @@
 import styles from "./ErrorModal.module.css";
 import Card from "../UI/Card";
-import Button from "./Button";
-
+import Button from "../UI/Button";
 const ErrorModal = (props) => {
   return (
     <div>
@@ -11,10 +10,10 @@ const ErrorModal = (props) => {
           <h2>{props.title}</h2>
         </header>
         <div className={styles.content}>
-          <p>{props.message}</p>
+          <p> {props.message}</p>
         </div>
         <footer className={styles.footer}>
-          <Button onClick={props.onConfirm}>OK</Button>
+          <Button onClick={props.onConfirm}>Ok</Button>
         </footer>
       </Card>
     </div>
